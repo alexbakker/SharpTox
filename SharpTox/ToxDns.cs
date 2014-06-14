f448a8972c74a9a250f52f70c4a12a6d4c9a2774
Imperative <admin@impy.me>
2014-06-14 17:15:01 +0200

    Added the dns3 functions, ready for testing

---
+++ SharpTox/ToxDns.cs
@@ -0,0 +1,63 @@
+﻿#pragma warning disable 1591
+
+using System;
+
+namespace SharpTox
+{
+    public class ToxDns
+    {
+        private IntPtr tox_dns3;
+
+        private Object obj;
+
+        public ToxDns(string public_key)
+        {
+            obj = new Object();
+
+            tox_dns3 = ToxDnsFunctions.New(public_key);
+
+            if (tox_dns3 == IntPtr.Zero)
+                throw new Exception("Could not create a new tox_dns3 instance with the provided public_key");
+        }
+
+        public void Kill()
+        {
+            lock (obj)
+            {
+                if (tox_dns3 == IntPtr.Zero)
+                    throw null;
+
+                ToxDnsFunctions.Kill(tox_dns3);
+            }
+        }
+
+        public string GenerateDns3String(string name)
+        {
+            lock (obj)
+            {
+                if (tox_dns3 == IntPtr.Zero)
+                    throw null;
+
+                return ToxDnsFunctions.GenerateDns3String(tox_dns3, name);
+            }
+        }
+
+        public string DecryptDns3TXT(string dns3_string)
+        {
+            lock (obj)
+            {
+                if (tox_dns3 == IntPtr.Zero)
+                    throw null;
+
+                return ToxDnsFunctions.DecryptDns3TXT(tox_dns3, dns3_string);
+            }
+        }
+
+        public IntPtr GetPointer()
+        {
+            return tox_dns3;
+        }
+    }
+}
+
+#pragma warning restore 1591
---
+++ SharpTox/ToxDnsFunctions.cs
@@ -0,0 +1,60 @@
+﻿#pragma warning disable 1591
+
+
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SharpTox
+{
+    public static class ToxDnsFunctions
+    {
+        const string dll = "libtoxav";
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr tox_dns3_new(byte[] public_key);
+        public static IntPtr New(string public_key)
+        {
+            return tox_dns3_new(ToxTools.StringToHexBin(public_key));
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void tox_dns3_kill(IntPtr dns3_object);
+        public static void Kill(IntPtr dns3_object)
+        {
+            tox_dns3_kill(dns3_object);
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int tox_generate_dns3_string(IntPtr dns3_object, byte[] str, ushort str_max_len, byte[] name, byte name_len);
+        public static string GenerateDns3String(IntPtr dns3_object, string name)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(name);
+            byte[] result = new byte[bytes.Length];
+
+            int length = tox_generate_dns3_string(dns3_object, result, (ushort)result.Length, bytes, (byte)bytes.Length);
+
+            if (length != -1)
+                return Encoding.UTF8.GetString(result);
+            else
+                throw new Exception("Failed to generate a dns3 string");
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int tox_decrypt_dns3_TXT(IntPtr dns3_object, byte[] tox_id, byte[] id_record, uint id_record_len);
+        public static string DecryptDns3TXT(IntPtr dns3_object, string id_record)
+        {
+            byte[] id = new byte[32 + sizeof(uint) + sizeof(ushort)];
+            byte[] id_record_bytes = Encoding.UTF8.GetBytes(id_record);
+
+            int result = tox_decrypt_dns3_TXT(dns3_object, id, id_record_bytes, (uint)id_record_bytes.Length);
+
+            if (result == 0)
+                return ToxTools.HexBinToString(id);
+            else
+                throw new Exception("Could not decrypt and decode id_record");
+        }
+    }
+}
+
+#pragma warning restore 1591
