247039860aaff24aace0f4bfcaf3911331fdffab
Imperative <admin@impy.me>
2014-08-18 22:02:36 +0200

    Some (fairly useless) documentation

--- SharpTox/Core/ToxEnums.cs
+++ SharpTox/Core/ToxEnums.cs
@@ -1,54 +1,158 @@
-﻿#pragma warning disable 1591
-
-namespace SharpTox.Core
+﻿namespace SharpTox.Core
 {
+    /// <summary>
+    /// Errors while adding a friend.
+    /// </summary>
     public enum ToxAFError
     {
+        /// <summary>
+        /// Message length is too long.
+        /// </summary>
         TooLong = -1,
+
+        /// <summary>
+        /// No message.
+        /// </summary>
         NoMessage = -2,
+
+        /// <summary>
+        /// Trying to add our own key.
+        /// </summary>
         OwnKey = -3,
+
+        /// <summary>
+        /// Friend already added or friend request already sent.
+        /// </summary>
         AlreadySent = -4,
+
+        /// <summary>
+        /// Unknown error.
+        /// </summary>
         Unknown = -5,
+
+        /// <summary>
+        /// Bad checksum in the address.
+        /// </summary>
         BadChecksum = -6,
+
+        /// <summary>
+        /// Friend was already added but the nospam was different.
+        /// </summary>
         SetNewNospam = -7,
+
+        /// <summary>
+        /// Increasing the friend list size failed.
+        /// </summary>
         NoMem = -8
     }
 
+    /// <summary>
+    /// UserStatus of a tox friend.
+    /// </summary>
     public enum ToxUserStatus
     {
+        /// <summary>
+        /// No status.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// Away.
+        /// </summary>
         Away,
+
+        /// <summary>
+        /// Busy.
+        /// </summary>
         Busy,
+
+        /// <summary>
+        /// Invalid status.
+        /// </summary>
         Invalid
     }
 
+    /// <summary>
+    /// Connection status of a friend.
+    /// </summary>
     public enum ToxUserConStatus
     {
+        /// <summary>
+        /// Not connected.
+        /// </summary>
         Offline,
+
+        /// <summary>
+        /// Connected.
+        /// </summary>
         Online
     }
 
+    /// <summary>
+    /// Controls to be used for file transfers.
+    /// </summary>
     public enum ToxFileControl
     {
+        /// <summary>
+        /// Accept a file send request.
+        /// </summary>
         Accept,
+
+        /// <summary>
+        /// Pause a file transfer.
+        /// </summary>
         Pause,
+
+        /// <summary>
+        /// Kill a file transfer.
+        /// </summary>
         Kill,
+
+        /// <summary>
+        /// Fully received/sent a file.
+        /// </summary>
         Finished,
+
+        /// <summary>
+        /// Resume a paused file transfer.
+        /// </summary>
         ResumeBroken
     }
 
+    /// <summary>
+    /// Changes that can occur in a group chat.
+    /// </summary>
     public enum ToxChatChange
     {
+        /// <summary>
+        /// A new peer joined the group.
+        /// </summary>
         PeerAdd,
+
+        /// <summary>
+        /// A peer left the group.
+        /// </summary>
         PeerDel,
+
+        /// <summary>
+        /// A peer changed its name.
+        /// </summary>
         PeerName
     }
 
+    /// <summary>
+    /// Tox key type.
+    /// </summary>
     public enum ToxKeyType
     {
+        /// <summary>
+        /// A public tox key.
+        /// </summary>
         Public,
+
+        /// <summary>
+        /// A secret tox key.
+        /// </summary>
         Secret
     }
 }
-
-#pragma warning restore 1591
