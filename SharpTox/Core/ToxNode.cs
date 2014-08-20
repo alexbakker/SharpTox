409bf7dcc16148cc767fa3757c532bedca90f481
Imperative <admin@impy.me>
2014-08-20 21:13:05 +0200

    Function that grabs the latest node list
    This is experimental. It'll likely be removed in a future version of
    SharpTox.

--- SharpTox/Core/ToxNode.cs
+++ SharpTox/Core/ToxNode.cs
@@ -1,4 +1,8 @@
-﻿namespace SharpTox.Core
+﻿using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SharpTox.Core
 {
     /// <summary>
     /// Represents a tox node.
@@ -32,6 +36,67 @@
             Address = address;
             Port = port;
             PublicKey = public_key;
+        }
+        
+        /// <summary>
+        /// Parses and returns an array of nodes grabbed from https://wiki.tox.im/Nodes. Only use this if you're desperate.
+        /// </summary>
+        /// <returns></returns>
+        public static ToxNode[] GetNodes()
+        {
+            WebClient client = new WebClient();
+            string content = "";
+
+            try
+            {
+                content = client.DownloadString("https://wiki.tox.im/Nodes");
+                client.Dispose();
+            }
+            catch
+            {
+                client.Dispose();
+                return new ToxNode[0];
+            }
+
+            var list = new List<ToxNode>();
+            int index = content.IndexOf("<table");
+            string table = content.Substring(index, content.IndexOf("</table") - index + "</table".Length);
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] != '<')
+                    continue;
+
+                if (string.Concat(table[i], table[i+1], table[i+2]) == "<tr")
+                {
+                    index = table.IndexOf(">", i + 3) + 1;
+                    string row = table.Substring(index, table.IndexOf("</tr>", i) - index).Replace("\n", "");
+
+                    if (row.StartsWith("<th"))
+                        continue;
+
+                    string[] cells = new string[7];
+                    int count = 0;
+                    for (int j = 0; j < row.Length; j++)
+                    {
+                        if (row[j] != '<')
+                            continue;
+
+                        if (string.Concat(row[j], row[j + 1], row[j + 2], row[j + 3]) == "<td>")
+                        {
+                            index = row.IndexOf("<td>", j) + 4;
+                            string cell = row.Substring(index, row.IndexOf("</td>", j) - index);
+                            cells[count] = cell;
+
+                            count++;
+                        }
+                    }
+
+                    list.Add(new ToxNode(cells[0], int.Parse(cells[2]), new ToxKey(ToxKeyType.Public, cells[3])));
+                }
+            }
+
+            return list.ToArray();
         }
     }
 }
