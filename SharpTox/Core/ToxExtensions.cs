94792ec53089509665e013448fd578a7ff7e45c6
Andrius Bentkus <andrius.bentkus@gmail.com>
2014-12-21 22:37:56 +0200

    Add extension for SendData.

--- SharpTox/Core/ToxExtensions.cs
+++ SharpTox/Core/ToxExtensions.cs
@@ -4,6 +4,10 @@ namespace SharpTox.Core
 {
     public static class ToxExtensions
     {
+        public static bool SendData(this ToxFileSender sender, byte[] data)
+        {
+            return sender.SendData(new ArraySegment<byte>(data, 0, data.Length));
+        }
     }
 }
 
