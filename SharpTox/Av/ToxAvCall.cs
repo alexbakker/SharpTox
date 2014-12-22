dcd518d1a839f153efd7d565a118d61e283531c1
Andrius Bentkus <andrius.bentkus@gmail.com>
2014-12-22 16:32:13 +0200

    Move some more functions to ToxAvCall.

--- SharpTox/Av/ToxAvCall.cs
+++ SharpTox/Av/ToxAvCall.cs
@@ -24,6 +24,106 @@ namespace SharpTox.Av
 
             return ToxAvFunctions.Answer(ToxAv.Handle, Index, ref settings);
         }
+
+        /// <summary>
+        /// Hangs up an in-progress call.
+        /// </summary>
+        /// <returns></returns>
+        public ToxAvError Hangup()
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.Hangup(ToxAv.Handle, Index);
+        }
+
+        /// <summary>
+        /// Rejects an incoming call.
+        /// </summary>
+        /// <param name="reason"></param>
+        /// <returns></returns>
+        public ToxAvError Reject(string reason)
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.Reject(ToxAv.Handle, Index, reason);
+        }
+
+        /// <summary>
+        /// Stops a call and terminates the transmission without notifying the remote peer.
+        /// </summary>
+        /// <returns></returns>
+        public ToxAvError Stop()
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.StopCall(ToxAv.Handle, Index);
+        }
+
+        /// <summary>
+        /// Checks whether a certain capability is supported.
+        /// </summary>
+        /// <param name="capability"></param>
+        /// <returns></returns>
+        public bool CapabilitySupported(ToxAvCapabilities capability)
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.CapabilitySupported(ToxAv.Handle, Index, capability) == 1;
+        }
+
+        /// <summary>
+        /// Sends an encoded audio frame.
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <param name="frameSize"></param>
+        /// <returns></returns>
+        public ToxAvError SendAudio(byte[] frame, int frameSize)
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.SendAudio(ToxAv.Handle, Index, frame, (uint)frameSize);
+        }
+
+        /// <summary>
+        /// Encodes an audio frame.
+        /// </summary>
+        /// <param name="dest"></param>
+        /// <param name="destMax"></param>
+        /// <param name="frames"></param>
+        /// <param name="perframe"></param>
+        /// <returns></returns>
+        public int PrepareAudioFrame(byte[] dest, int destMax, short[] frames, int perframe) //TODO: use 'out' keyword to get the encoded frame
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.PrepareAudioFrame(ToxAv.Handle, Index, dest, destMax, frames, perframe);
+        }
+
+        /// <summary>
+        /// Retrieves the state of a call.
+        /// </summary>
+        /// <returns></returns>
+        public ToxAvCallState State
+        {
+            get
+            {
+                ToxAv.CheckDisposed();
+
+                return ToxAvFunctions.GetCallState(ToxAv.Handle, Index);
+            }
+        }
+
+        /// <summary>
+        /// Changes the type of an in-progress call
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public ToxAvError ChangeSettings(ToxAvCodecSettings settings)
+        {
+            ToxAv.CheckDisposed();
+
+            return ToxAvFunctions.ChangeSettings(ToxAv.Handle, Index, ref settings);
+        }
     }
 }
 
