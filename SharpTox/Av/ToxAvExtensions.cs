a9f647911bd08f9e849dc9eb20876747906a2c70
Andrius Bentkus <andrius.bentkus@gmail.com>
2014-12-22 16:35:20 +0200

    Add Answer extensions.
    Makes it easier to answer, just uses the default settings.

--- SharpTox/Av/ToxAvExtensions.cs
+++ SharpTox/Av/ToxAvExtensions.cs
@@ -7,6 +7,14 @@ namespace SharpTox.Av
     /// </summary>
     public static class ToxAvExtensions
     {
+        /// <summary>
+        /// Answer a call with default codec settings.
+        /// </summary>
+        /// <param name="call">Call.</param>
+        public static ToxAvError Answer(this ToxAvCall call)
+        {
+            return call.Answer(ToxAv.DefaultCodecSettings);
+        }
     }
 }
 
