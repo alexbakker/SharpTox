2a7a4690f9ea64f6fceb127305fe932384e187ef
Impyy <alex@impy.me>
2015-08-18 17:57:13 +0200

    Added group constants and (error) enums

---
+++ SharpTox/Core/ToxGroupConstants.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTox.Core
+{
+    /// <summary>
+    /// A collection of constants for groupchats.
+    /// </summary>
+    public static class ToxGroupConstants
+    {
+        /// <summary>
+        /// Maximum length of a group topic.
+        /// </summary>
+        public const int MaxTopicLength = 512;
+
+        /// <summary>
+        /// Maximum length of a peer part message.
+        /// </summary>
+        public const int MaxPartLength = 128;
+
+        /// <summary>
+        /// Maximum length of a group name.
+        /// </summary>
+        public const int MaxGroupNameLength = 48;
+
+        /// <summary>
+        /// Maximum length of a group password.
+        /// </summary>
+        public const int MaxPasswordSize = 32;
+
+        /// <summary>
+        /// Number of bytes in a group chat ID.
+        /// </summary>
+        public const int GroupChatIdSize = 32;
+    }
+}
---
+++ SharpTox/Core/ToxGroupEnums.cs
@@ -0,0 +1,252 @@
+﻿namespace SharpTox.Core
+{
+    /// <summary>
+    /// Privacy states a group can have.
+    /// </summary>
+    public enum ToxGroupPrivacyState
+    {
+        /// <summary>
+        /// This group is public. Anyone can join the group using the chat ID.
+        /// </summary>
+        Public,
+
+        /// <summary>
+        /// This group is private. You can only be join this group via an invite from someone in your friend list.
+        /// </summary>
+        Private
+    }
+
+    /// <summary>
+    /// Roles peers of a group can have.
+    /// </summary>
+    public enum ToxGroupRole
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Founder,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Moderator,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        User,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Observer
+    }
+
+    public enum ToxErrorGroupNew
+    {
+        Ok,
+        TooLong,
+        Empty,
+        Privacy,
+        Init,
+        State,
+        Announce
+    }
+
+    public enum ToxErrorGroupJoin
+    {
+        Ok,
+        Init,
+        BadChatId,
+        TooLong
+    }
+
+    public enum ToxErrorGroupReconnect
+    {
+        Ok,
+        GroupNotFound
+    }
+
+    public enum ToxErrorGroupLeave
+    {
+        Ok,
+        GroupNotFound,
+        TooLong,
+        FailSend,
+        DeleteFail
+    }
+
+    public enum ToxErrorGroupSelfQuery
+    {
+        Ok,
+        GroupNotFound
+    }
+
+    public enum ToxErrorGroupSelfNameSet
+    {
+        Ok,
+        GroupNotFound,
+        TooLong,
+        Invalid,
+        Taken,
+        FailSend
+    }
+
+    public enum ToxErrorGroupSelfStatusGet
+    {
+        Ok,
+        GroupNotFound,
+        Invalid,
+        FailSend
+    }
+
+    public enum ToxErrorGroupPeerQuery
+    {
+        Ok,
+        GroupNotFound,
+        PeerNotFound
+    }
+
+    public enum ToxErrorGroupStateQueries
+    {
+        Ok,
+        GroupNotFound
+    }
+
+    public enum ToxErrorGroupTopicSet
+    {
+        Ok,
+        GroupNotFound,
+        TooLong,
+        Permissions,
+        FailCreate,
+        FailSend
+    }
+
+    public enum ToxErrorGroupSendMessage
+    {
+        Ok,
+        GroupNotFound,
+        TooLong,
+        Empty,
+        BadType,
+        Permissions,
+        FailSend
+    }
+
+    public enum ToxErrorGroupSendPrivateMessage
+    {
+        Ok,
+        GroupNotFound,
+        PeerNotFound,
+        TooLong,
+        Empty,
+        Permissions,
+        FailSend
+    }
+
+    public enum ToxErrorGroupInviteFriend
+    {
+        Ok,
+        GroupNotFound,
+        FriendNotFound,
+        InviteFail,
+        FailSend
+    }
+
+    public enum ToxErrorGroupInviteAccept
+    {
+        Ok,
+        BadInvite,
+        InitFailed,
+        TooLong
+    }
+
+    public enum ToxGroupJoinFail
+    {
+        NameTaken,
+        PeerLimit,
+        InvalidPassword,
+        Unknown
+    }
+
+    public enum ToxErrorGroupFounderSetPassword
+    {
+        Ok,
+        GroupNotFound,
+        Permissions,
+        TooLong,
+        FailSend
+    }
+
+    public enum ToxErrorGroupFounderSetPrivacyState
+    {
+        Ok,
+        GroupNotFound,
+        Invalid,
+        Permissions,
+        FailSet,
+        FailSend
+    }
+
+    public enum ToxErrorGroupFounderSetPeerLimit
+    {
+        Ok,
+        GroupNotFound,
+        Permissions,
+        FailSet,
+        FailSend
+    }
+
+    public enum ToxErrorGroupToggleIgnore
+    {
+        Ok,
+        GroupNotFound,
+        PeerNotFound
+    }
+
+    public enum ToxErrorGroupModSetRole
+    {
+        Ok,
+        GroupNotFound,
+        PeerNotFound,
+        Permissions,
+        Assignment,
+        FailAction
+    }
+
+    public enum ToxErrorGroupModRemovePeer
+    {
+        Ok,
+        GroupNotFound,
+        PeerNotFound,
+        Permissions,
+        FailAction,
+        FailSend
+    }
+
+    public enum ToxErrorGroupModRemoveBam
+    {
+        Ok,
+        GroupNotFound,
+        Permissions,
+        FailAction,
+        FailSend
+    }
+
+    public enum ToxGroupModEvent
+    {
+        Kick,
+        Ban,
+        Observer,
+        User,
+        Moderator
+    }
+
+    public enum ToxErrorGroupBanQuery
+    {
+        Ok,
+        GroupNotFound,
+        BadId
+    }
+}
